f95c863819afdb2caa3b2b2417fa83e9c929225b
Holly Fortener <hforten@purdue.edu>
2026-01-23 20:29:16 +0000

    wrote test_file and solution file for py1_ind_1_b

---
+++ source/Part_04_Python/M1/tasks/ind_1/b/test_cases.py
@@ -0,0 +1,20 @@
+note = " "
+labels = ["Device Name", "ID Number", 
+          "User Last Name", 
+          "User Title"]
+cases = {
+    1: {
+        "callable": "main",
+        "entries": ["Heart Monitor", 
+                    "101", 
+                    "doe", 
+                    "biomedical engineer" ],
+    },
+    2: {
+        "callable": "main",
+        "entries": ["Blood Pressure Monitor", 
+                    "102", 
+                    "smith", 
+                    "clinical researcher"],
+    },
+}
