9626d8724d9d7f68ca7ed279374d5b9f51c1adc3
pritchettk <kpritche@purdue.edu>
2026-03-06 21:12:28 +0000

    Updated autograding tests for EX1. Installed generic-grader fork.

--- source/Part_02_Excel/M1/tasks/pre_0/a/test_config.py
+++ source/Part_02_Excel/M1/tasks/pre_0/a/test_config.py
@@ -1,6 +1,5 @@
 from generic_grader.file import file_presence
-from generic_grader.excel import data_match_reference
-from generic_grader.excel import formulas_exist
+from generic_grader.excel import data_match_reference, formulas_match_reference, formulas_exist, chart_metadata_match_reference, data_series_exists, data_series_match_reference
 from generic_grader.utils.options import Options
 
 DUE_DATE = None  # Unused because we don't accept late pre-class tasks.
@@ -13,25 +12,71 @@ file_set_up_options = Options(required_files=REQUIRED_FILES)
 
 test_00_TestFilePresence = file_presence.build(
     Options(
-        weight=2,
+        weight=TOTAL_POINTS/7,
         required_files=REQUIRED_FILES,
     )
 )
 
-test_01_TestDataValues = data_match_reference.build(
-    Options(ref_module=REF_MODULE, 
-            sub_module=SUB_MODULE, 
-            entries=("F16", "F20"),
-            weight=3
-            kwargs={"sheet": "Sheet1"})
-    
-)
-
-test_02_TestFormulasExist = formulas_exist.build(
+test_01_TestDataSeriesExist = data_series_exists.build(
     Options(
         ref_module=REF_MODULE,
         sub_module=SUB_MODULE,
         entries=("F16", "F20"),
-        kwargs={"sheet": "Sheet1"}
+        weight=TOTAL_POINTS/7,
+        sheet="Sheet1",
+        series_require_formulas=True
+    )
+)
+
+test_02_TestDataValues = data_series_match_reference.build(
+    Options(ref_module=REF_MODULE, 
+            sub_module=SUB_MODULE, 
+            entries=("F16", "F20"),
+            weight=TOTAL_POINTS/7,
+            sheet="Sheet1"
+        )
+)
+
+
+test_03_TestDataSeriesExist = data_series_exists.build(
+    Options(
+        ref_module=REF_MODULE,
+        sub_module=SUB_MODULE,
+        entries=("G24", "G30"),
+        weight=TOTAL_POINTS/7,
+        sheet="Sheet1",
+        series_require_formulas=True
+    )
+)
+
+test_04_TestDataSeriesExist = data_series_exists.build(
+    Options(
+        ref_module=REF_MODULE,
+        sub_module=SUB_MODULE,
+        entries=("H24", "H30"),
+        weight=TOTAL_POINTS/7,
+        sheet="Sheet1",
+        series_require_formulas=True
+    )
+)
+
+test_05_TestDataValues = data_series_match_reference.build(
+    Options(ref_module=REF_MODULE,  
+            sub_module=SUB_MODULE, 
+            entries=("H24", "H30"),
+            weight=TOTAL_POINTS/7,
+            sheet="Sheet1",
+            series_require_formulas=False
+            )
+)
+
+test_06_ChartsMatchReference = chart_metadata_match_reference.build(
+    Options(
+        ref_module=REF_MODULE,
+        sub_module=SUB_MODULE,
+        entries=("Chart 1", "Chart 2"),
+        sheet="Sheet1",
+        weight=TOTAL_POINTS/7,
+        chart_ratio=.15
     )
 )
