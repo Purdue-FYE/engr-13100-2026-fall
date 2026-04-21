330c18308a7282e9d062c783a4f7870ba788f6c1
Kory Pritchett <kpritche@purdue.edu>
2026-04-21 13:37:53 +0000

    Update setup.sh for github pulling.

--- grader/setup.sh
+++ grader/setup.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 # Install additional packages
 # xvfb:
 #   - a virtual frame buffer that us to draw on the "screen" for turtle
@@ -17,6 +16,20 @@ apt-get install -y xvfb ghostscript tesseract-ocr tmux
 add-apt-repository ppa:deadsnakes/ppa
 apt-get update && apt-get install -y python3.13 python3.13-tk python3.13-venv
 
+mkdir -p /root/.ssh
+cp /autograder/source/deploy_key /root/.ssh/deploy_key
+chmod 600 /root/.ssh/deploy_key
+
+# Create an SSH config file telling git to use this key for GitHub
+cat << 'EOF' > /root/.ssh/config
+Host github.com
+    IdentityFile /root/.ssh/deploy_key
+    IdentitiesOnly yes
+EOF
+
+# Add GitHub to known_hosts to prevent "host key verification failed" errors
+ssh-keyscan -t rsa,ed25519 github.com >> /root/.ssh/known_hosts
+
 # Run the auto-grader in a python 3.13 virtual environment.  Otherwise we can't
 # install pytesseract because it depends on an up-to-date setuptools, but
 # updating setuptools outside of a virtual environment breaks Gradescope.
@@ -26,3 +39,11 @@ source env/bin/activate
 # Upgrade pip, then install wheel so we won't have to build other requirements.
 python3.13 -m pip install --upgrade pip wheel
 python3.13 -m pip install git+https://github.com/pritchettk/generic-grader git+https://github.com/pritchettk/gradescope-utils-extended pandas faker opencv-python
+
+# Clone autograder files from GitHub
+git clone --no-checkout --depth 1 --sparse git@github.com:your-organization/your-repo.git /autograder/my_grader_repo
+
+cd /autograder/my_grader_repo
+
+git sparse-checkout set
+git checkout main
