97994f34d318362050b9c233e3696f25d0c48834
Holly Fortener <hforten@purdue.edu>
2026-09-15 19:58:58 +0000

    Update documentation

---
+++ _downloads/6f9fe2533bd78dba5d258cc78306ed0a/00_hello_world.py
@@ -0,0 +1,14 @@
+"""
+ENGR 131
+Running Your First Python Script
+
+This program prints a message to the console.
+
+Learning Goals:
+- Understand how to run a Python script
+- Use the print() function to display output
+- Observe how Python executes code line by line
+"""
+
+# This line prints text to the console.
+print("Hello, World!")
