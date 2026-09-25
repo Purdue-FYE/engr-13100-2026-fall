923dcd26c507c23ede4e960d88ffe6e08dfe67a0
Holly Fortener <hforten@purdue.edu>
2026-09-25 15:48:19 +0000

    Update documentation

---
+++ _downloads/19256f18ef8faabee85374609ee85ef8/ENGR131_Python_Template.py
@@ -0,0 +1,23 @@
+"""
+Course Number: ENGR 13100
+
+Description:
+    Replace this line with a description of your program.
+
+Assignment Information:
+    Assignment:     #.#.#
+    Author:         Name, login@purdue.edu
+    Section:        ###
+    Team:           ## 
+
+"""
+
+""" Write any import statements here (and delete this line)."""
+
+
+def main():
+    """Write your code here (and delete this line)."""
+
+
+if __name__ == "__main__":
+    main()
---
+++ _downloads/b97137d7cbb394f0dd0ecd68565cd0ff/hello_template.py
@@ -0,0 +1,24 @@
+"""
+Course Number: ENGR 13100
+Semester: Fall 2026
+
+Description:
+    Template file for 8.2.1. ICA Debugging/Autograder Practice
+
+Assignment Information:
+    Assignment:     8.2.1. ICA_1
+    Author:         Holly Fortener, hforten@purdue.edu
+    Date:           09/01/2026
+
+"""
+
+def main():
+    # Prompt the user (you!) for input and store it in a variable called name
+    name = input("  What is your name?")
+
+    # Print "Hello, " followed by your name, and then an exclamation mark
+    print("Hello")
+    print(name + "!")
+
+if __name__ == "__main__":
+    main()
